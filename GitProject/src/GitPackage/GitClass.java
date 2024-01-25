e032c8b1bd357b64e85591ee3271a14542eb8b2b
Empiric-52 <Empiric-52@DESKTOP-O2G7T0E>
2024-01-25 16:57:55 +0530

    Modified Code

--- GitProject/src/GitPackage/GitClass.java
+++ GitProject/src/GitPackage/GitClass.java
@@ -7,14 +7,8 @@ public class GitClass {
 
 		public static void main(String[] args) throws Exception {
 			
-			//Setting the Property of ChromeBrowser and Passing ChromeDriver Path 
-			// Setting the Property of Chrome Browser and Passing ChromeDriver Path
-					System.setProperty("webdriver.chrome.driver", "C://Users//Empiric-52//eclipse-workspace//chromedriver.exe");
-					WebDriver driver = new ChromeDriver();	
-					driver.manage().window().maximize();
-					driver.get("https://www.facebook.com/");
-					driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();     //click on "Create New Account" Tab from Facebook Login Webpage
-					Thread.sleep(2000);
+			System.out.println("Hello Maven");
+			System.out.println("Hello Shammi");
 	}
 
 }
