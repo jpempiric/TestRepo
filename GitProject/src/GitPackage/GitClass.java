874e27984c89cc603b445d6c2e2f49a8b4e21f77
Empiric-52 <Empiric-52@DESKTOP-O2G7T0E>
2024-01-25 17:56:58 +0530

    Ad Print Statement

--- GitProject/src/GitPackage/GitClass.java
+++ GitProject/src/GitPackage/GitClass.java
@@ -8,6 +8,11 @@ public class GitClass {
 		public static void main(String[] args) throws Exception {
 			
 			System.out.println("Hello Maven");
+			System.out.println("Namaste raam");
+			
+			
+			
+			System.out.println("Om, Jay, Jagdish");
 			
 			
 	}
